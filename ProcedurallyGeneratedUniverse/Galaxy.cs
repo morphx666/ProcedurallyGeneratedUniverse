efb83e0a1b98d5504f1eeee80af16e2b18bf34d9
Xavier Flix <xavier@xfx.net>
2020-01-26 02:54:42 -0400

    Add project files.

---
+++ ProcedurallyGeneratedUniverse/Galaxy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace ProcedurallyGeneratedUniverse {
+    public class Galaxy {
+        private int sectorSize = 16;
+        private int sectorSize2;
+        private int sectorSize4;
+        public PointF Offset = new PointF(0.0f, 0.0f);
+
+        public Galaxy() {
+            sectorSize2 = sectorSize / 2;
+            sectorSize4 = sectorSize / 4;
+        }
+
+        public void Render(Graphics g, Rectangle r) {
+            g.Clear(Color.Black);
+
+            float w = r.Width / sectorSize;
+            float h = r.Height / sectorSize;
+
+            Point screenSector = new Point(0, 0);
+
+            for(screenSector.X = -(int)sectorSize2; screenSector.X < w; screenSector.X++) {
+                for(screenSector.Y = -(int)sectorSize2; screenSector.Y < h; screenSector.Y++) {
+                    UInt32 seed1 = (UInt32)(Offset.X + screenSector.X);
+                    UInt32 seed2 = (UInt32)(Offset.Y + screenSector.Y);
+
+                    StarSystem star = new StarSystem(seed1, seed2);
+                    if(star.Exists) {
+                        g.FillEllipse(star.ForeColor,
+                                        screenSector.X * sectorSize + sectorSize2,
+                                        screenSector.Y * sectorSize + sectorSize2,
+                                        (float)(star.Diameter / sectorSize4),
+                                        (float)(star.Diameter / sectorSize4));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ProcedurallyGeneratedUniverse/StarSystem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+
+namespace ProcedurallyGeneratedUniverse {
+    public class StarSystem {
+        private Color[] colors = {Color.White, Color.Blue, Color.Green, Color.Red,
+                                  Color.Violet, Color.Yellow, Color.Cyan, Color.DeepSkyBlue,
+                                  Color.Chocolate, Color.DarkSlateBlue, Color.LightGreen};
+        private UInt32 procGen = 0;
+
+        public bool Exists = false;
+        public double Diameter = 0.0;
+        public Brush ForeColor = Brushes.White;
+
+        public StarSystem(UInt32 x, UInt32 y, bool generateFullSystem = false) {
+            // Set seed based on location of star system
+            procGen = (x & 0xFFFF) << 16 | (y & 0xFFFF);
+
+            // Not all locations contain a system
+            Exists = (RndInt(0, 30) == 1);
+            if(!Exists) return;
+
+            // Generate Star
+            Diameter = RndDouble(10.0, 40.0);
+            ForeColor = new SolidBrush(colors[RndInt(0, colors.Length)]);
+
+            // When viewing the galaxy map, we only care about the star
+            // so abort early
+            if(!generateFullSystem) return;
+
+            // If we are viewing the system map, we need to generate the
+            // full system
+
+            // TODO
+        }
+
+        private double RndDouble(double min, double max) {
+            return ((double)Rnd() / 0x7FFFFFFF) * (max - min) + min;
+        }
+
+        private int RndInt(int min, int max) {
+            return (int)(Rnd() % (max - min)) + min;
+        }
+
+        // https://en.wikipedia.org/wiki/Lehmer_random_number_generator
+        private UInt32 Rnd() {
+            procGen += 0xe120fc15;
+            UInt64 tmp = (UInt64)procGen * 0x4a39b70d;
+            UInt32 m1 = (UInt32)((tmp >> 32) ^ tmp);
+            tmp = (UInt64)m1 * 0x12fad5c9;
+            UInt32 m2 = (UInt32)((tmp >> 32) ^ tmp);
+            return m2;
+        }
+    }
+}
